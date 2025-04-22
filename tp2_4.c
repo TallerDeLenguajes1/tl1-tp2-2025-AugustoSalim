849469d3d2deb5f6e8c1a8d19b7489d6760c634d
AugustoSalim <augustosalim98@gmail.com>
2025-04-21 23:33:56 -0300

    Ejercicio 4 completado y revisado. TP COMPLETADO

--- tp2_4.c
+++ tp2_4.c
@@ -10,6 +10,10 @@ typedef struct
     char *tipo_cpu;     // Tipo de procesador (apuntador a cadena de caracteres)
 } compu;
 
+void listarPCs(compu pcs[], int cantidad);
+void mostrarMasVieja(compu pcs[], int cantidad);
+void mostrarMasVeloz(compu pcs[], int cantidad);
+
 int main(){
     srand(time(NULL));
 
@@ -21,11 +25,62 @@ int main(){
     for (int i = 0; i < 5; i++)
     {
         pcs[i].velocidad = 1 + rand() % 3;
-        pcs[i].anio = 2015 + rand() % 2024;
+        pcs[i].anio = 2015 + rand() % 10;
         pcs[i].cantidad_nucleos = 1 + rand() % 8;
         pcs[i].tipo_cpu = tipos[rand() % 6];
     }
     
+    listarPCs(pcs, 5);
+    mostrarMasVieja(pcs, 5);
+    mostrarMasVeloz(pcs, 5);
 
     return 0;
+}
+
+void listarPCs(compu pcs[], int cantidad){
+    printf("\n--- LISTADO DE PCs ---\n");
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("\nPC N# %d\n", i+1);
+        printf("Velocidad: %d GHz\n", pcs[i].velocidad);
+        printf("Anio: %d\n", pcs[i].anio);
+        printf("Cantidad de Nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("Tipo de CPU: %s\n", pcs[i].tipo_cpu);
+    }
+}
+
+void mostrarMasVieja(compu pcs [], int cantidad){
+    int menor = 3000, indice = 0;
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (pcs[i].anio < menor)
+        {
+            menor = pcs[i].anio;
+            indice = i;
+        }
+    }
+    printf("\nLa pc mas vieja fue:\n");
+    printf("\nPC N# %d\n", indice+1);
+    printf("Velocidad: %d GHz\n", pcs[indice].velocidad);
+    printf("Anio: %d\n", pcs[indice].anio);
+    printf("Cantidad de Nucleos: %d\n", pcs[indice].cantidad_nucleos);
+    printf("Tipo de CPU: %s\n", pcs[indice].tipo_cpu);
+}
+
+void mostrarMasVeloz(compu pcs [], int cantidad){
+    int mayor = 0, indice = 0;
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (pcs[i].anio > mayor)
+        {
+            mayor = pcs[i].anio;
+            indice = i;
+        }
+    }
+    printf("\nLa pc mas veloz fue:\n");
+    printf("\nPC N# %d\n", indice+1);
+    printf("Velocidad: %d GHz\n", pcs[indice].velocidad);
+    printf("Anio: %d\n", pcs[indice].anio);
+    printf("Cantidad de Nucleos: %d\n", pcs[indice].cantidad_nucleos);
+    printf("Tipo de CPU: %s\n", pcs[indice].tipo_cpu);
 }
